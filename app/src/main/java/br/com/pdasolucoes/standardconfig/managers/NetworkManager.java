723a3d63ee97fc688502d608d46667f3b9f0bd73
Vinicius Novais <v.novais@outlook.com>
2023-05-09 16:56:15 -0300

    ajustes passagem de sharedpreferences

--- app/src/main/java/br/com/pdasolucoes/standardconfig/managers/NetworkManager.java
+++ app/src/main/java/br/com/pdasolucoes/standardconfig/managers/NetworkManager.java
@@ -172,6 +172,7 @@ public class NetworkManager {
                     ConfigurationHelper.savePreference(ConfigurationHelper.ConfigurationEntry.Token, activity.getIntent().getExtras().getString(ConfigurationHelper.ConfigurationEntry.Token.getKeyName(), ""));
                     ConfigurationHelper.savePreference(ConfigurationHelper.ConfigurationEntry.RefreshToken, activity.getIntent().getExtras().getString(ConfigurationHelper.ConfigurationEntry.RefreshToken.getKeyName(), ""));
                     ConfigurationHelper.savePreference(ConfigurationHelper.ConfigurationEntry.UserPassword, activity.getIntent().getExtras().getString(ConfigurationHelper.ConfigurationEntry.UserPassword.getKeyName(), ""));
+                    ConfigurationHelper.savePreference(ConfigurationHelper.ConfigurationEntry.TimeOutSession, activity.getIntent().getExtras().getInt(ConfigurationHelper.ConfigurationEntry.TimeOutSession.getKeyName(), 0));
 
                     ConfigurationHelper.savePreference(ConfigurationHelper.ConfigurationEntry.ServerAddress, activity.getIntent().getExtras().getString(ConfigurationHelper.ConfigurationEntry.ServerAddress.getKeyName(), ""));
                     ConfigurationHelper.savePreference(ConfigurationHelper.ConfigurationEntry.ServerAddressApi, activity.getIntent().getExtras().getString(ConfigurationHelper.ConfigurationEntry.ServerAddressApi.getKeyName(), ""));
