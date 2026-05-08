ca68f605f2744fb5cceb0c14b969f26e629d8b04
viniciusnovais <v.novais@outlook.com>
2026-05-08 16:43:24 -0300

    não exibir mensagem

--- app/src/main/java/br/com/pdasolucoes/standardconfig/network/SendRequestTask.java
+++ app/src/main/java/br/com/pdasolucoes/standardconfig/network/SendRequestTask.java
@@ -272,7 +272,7 @@ public class SendRequestTask extends AsyncTaskRunner<Void, Void, Object> {
                 } else if (responseCode == HttpsURLConnection.HTTP_BAD_REQUEST) {
                     inputStream = connection.getErrorStream(); // <- lê o corpo de erro (JSON)
                 }else if(responseCode == HttpsURLConnection.HTTP_UNAUTHORIZED){
-                    inputStream = connection.getErrorStream();
+                    return MessageConfiguration.ExceptionError;
                 } else {
                     MessageConfiguration.ExceptionError.setExceptionErrorMessage("Erro: " + responseCode + connection.getContentType());
                     return MessageConfiguration.ExceptionError;
